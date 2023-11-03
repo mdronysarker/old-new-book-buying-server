01e7913eb12a3307839e3feb355beeb1ded3ea1e
Abu-Sahad <abusahad603@gmail.com>
2023-11-03 23:35:17 +0600

    client site and user site added

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.feafdbq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7mh4myd.mongodb.net/?retryWrites=true&w=majority`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
--- user.js
+++ user.js
@@ -19,8 +19,6 @@ async function run() {
                 const result = await usersCollection.insertOne(user);
                 res.send(result)
             })
-
-
         // Send a ping to confirm a successful connection
         await client.db("user").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
